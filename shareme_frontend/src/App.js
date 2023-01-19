4515ce31308b917e60de1ebe496657d9296b83ee
unknown <amrebrahem226@gmail.com>
2023-01-20 01:22:21 +0200

    fix redirect

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Login } from "./componenets";
 import Home from "./containers/Home";
 
 function App() {
-  useEffect(() => {
-    const user = localStorage.getItem("user")
-      ? JSON.parse(localStorage.getItem("user"))
-      : undefined;
+  const navigate = useNavigate();
 
-    if (!user) Navigate("/login");
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
   }, []);
 
   return (
