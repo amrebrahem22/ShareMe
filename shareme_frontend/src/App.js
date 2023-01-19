04958e5ad539ef321f33689304a574a0521bb606
unknown <amrebrahem226@gmail.com>
2023-01-20 00:54:59 +0200

    redirect if not logged in

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom'
-import {Login} from './componenets';
-import Home from './containers/Home';
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./componenets";
+import Home from "./containers/Home";
+import { fetchUser } from "./utils/data";
 
 function App() {
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) Navigate("/login");
+  }, []);
+
   return (
     <div>
       <Routes>
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='*' element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="*" element={<Home />} />
       </Routes>
     </div>
   );
