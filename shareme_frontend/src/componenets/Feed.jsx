293fd604407b837273ce498ac0fc787ef4128e88
unknown <amrebrahem226@gmail.com>
2023-01-19 22:09:16 +0200

    user profile

--- shareme_frontend/src/componenets/Feed.jsx
+++ shareme_frontend/src/componenets/Feed.jsx
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
-import { client } from '../client';
-import Spinner from './Spinner';
-import {searchQuery, feedQuery} from '../utils/data';
-import {MasonryLayout} from './';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
+import { client } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
 
 const Feed = () => {
-  const [pins, setPins] = useState()
-  const [loading, setLoading] = useState(false)
-  const {categoryId} = useParams();
+  const [pins, setPins] = useState();
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
 
   useEffect(() => {
     if (categoryId) {
@@ -25,23 +25,16 @@ const Feed = () => {
       client.fetch(feedQuery).then((data) => {
         setPins(data);
         setLoading(false);
-        console.log('data:', data);
-
       });
     }
   }, [categoryId]);
-
+  const ideaName = categoryId || "new";
   if (loading) {
     return (
-      <Spinner message={`We are adding new ideas to your feed!`} />
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
     );
   }
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
 
-  return (
-    <div>
-      {pins && (<MasonryLayout pins={pins} />)}
-    </div>
-  )
-}
-
-export default Feed
+export default Feed;
--- shareme_frontend/src/componenets/Sidebar.jsx
+++ shareme_frontend/src/componenets/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RiHomeFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
-
+import { categories } from "../utils/data";
 import logo from "../assets/logo.png";
 
 const isNotActiveStyle =
@@ -10,14 +10,6 @@ const isNotActiveStyle =
 const isActiveStyle =
   "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize";
 
-const categories = [
-  { name: "Animals" },
-  { name: "Wallpapers" },
-  { name: "Photography" },
-  { name: "Gaming" },
-  { name: "Coding" },
-  { name: "Other" },
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -58,6 +50,10 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+              />
               {category.name}
             </NavLink>
           ))}
